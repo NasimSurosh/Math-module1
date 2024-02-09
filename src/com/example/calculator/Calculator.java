1eb5a952f8a7445a6ce2d9f1a38ca512ef0dd56e
NasimSursh <suroshzahiri@gmail.com>
2024-02-09 14:44:20 +0100

    module main class

---
+++ src/com/example/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.example.calculator;
+import com.example.math.Addition;
+
+public class Calculator {
+
+  public static void main(String[] args) {
+    
+    Addition addition = new Addition();
+    addition.add(4,5);
+
+  }
+
+}
